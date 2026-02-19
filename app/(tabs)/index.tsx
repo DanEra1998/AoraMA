1867271f2c988bddd3e66173b98c876dbd52a553
Daniel Herrera <danielherrera@Mac.lan>
2026-02-18 23:26:43 -0800

    addition of background, place holder search, and main logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
-
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 export default function HomeScreen() {
+  // Feb 18, 2026, we end at 1:06:40
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl text-primary font-pextrabold">
-        Welcome Daniel!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image
+          source={icons.logo}
+          className="w-12 h-10 mt-20 mb-5 mx-auto"
+        ></Image>
+        <View className="flex-1 mt-5">
+          <SearchBar />
+        </View>
+      </ScrollView>
     </View>
   );
 }
